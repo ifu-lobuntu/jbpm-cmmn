16c31902f9b74aa32ee14fb7237d5556ee417f86
Ampie Barnard <ampieb@gmail.com>
2015-04-18 11:42:56 +0200

    Testing

--- jbpm-cmmn-casefile/jbpm-cmmn-casefile-jpa/src/main/java/org/jbpm/cmmn/casefile/jpa/JpaCaseFilePersistence.java
+++ jbpm-cmmn-casefile/jbpm-cmmn-casefile-jpa/src/main/java/org/jbpm/cmmn/casefile/jpa/JpaCaseFilePersistence.java
@@ -27,167 +27,169 @@ import org.kie.internal.runtime.manager.context.EmptyContext;
  * memory leaks
  * 
  */
-public class JpaCaseFilePersistence implements SubscriptionPersistenceContext<JpaCaseSubscriptionInfo, JpaCaseFileItemSubscriptionInfo>,
-		CaseFilePersistence {
-	public static final String ENV_NAME = JpaCaseFilePersistence.class.getName() + "VAR";
-	static ThreadLocal<EntityManager> em = new ThreadLocal<EntityManager>();
-	protected EntityManagerFactory emf;
-	protected boolean startedTransaction = false;
-	protected Stopwatch stopwatch = new Stopwatch(getClass());
-	private UserTransaction transaction;
-	private RuntimeManager runtimeManager;
+public class JpaCaseFilePersistence implements SubscriptionPersistenceContext<JpaCaseSubscriptionInfo, JpaCaseFileItemSubscriptionInfo>, CaseFilePersistence {
+    public static final String ENV_NAME = JpaCaseFilePersistence.class.getName() + "VAR";
+    static ThreadLocal<EntityManager> em = new ThreadLocal<EntityManager>();
+    protected EntityManagerFactory emf;
+    protected boolean startedTransaction = false;
+    protected Stopwatch stopwatch = new Stopwatch(getClass());
+    private UserTransaction transaction;
+    private RuntimeManager runtimeManager;
 
-	public JpaCaseFilePersistence(EntityManagerFactory emf, RuntimeManager rm) {
-		this.runtimeManager = rm;
-		this.emf = emf;
-	}
+    public JpaCaseFilePersistence(EntityManagerFactory emf, RuntimeManager rm) {
+        this.runtimeManager = rm;
+        this.emf = emf;
+    }
 
-	@Override
-	public Object getDelegate() {
-		return getEntityManager().getDelegate();
-	}
+    @Override
+    public Object getDelegate() {
+        return getEntityManager().getDelegate();
+    }
 
-	@Override
-	public void start() {
-		try {
-			if (em.get() != null && em.get().isOpen()) {
-				em.get().close();
-				em.set(null);
-			}
-			startOrJoinTransaction();
-		} catch (Exception e) {
-			throw convertException(e);
-		}
-	}
+    @Override
+    public void start() {
+        try {
+            if (em.get() != null && em.get().isOpen()) {
+                em.get().close();
+                em.set(null);
+            }
+            startOrJoinTransaction();
+        } catch (Exception e) {
+            throw convertException(e);
+        }
+    }
 
+    protected void startOrJoinTransaction() {
+        try {
+            if (!isTransactionActive()) {
+                this.startedTransaction = true;
+                getTransaction().begin();
+            }
+            getEntityManager().joinTransaction();
+        } catch (Exception e) {
+            throw convertException(e);
+        }
+    }
 
-	protected void startOrJoinTransaction() {
-		try {
-			if (!isTransactionActive()) {
-				this.startedTransaction = true;
-				getTransaction().begin();
-			}
-			getEntityManager().joinTransaction();
-		} catch (Exception e) {
-			throw convertException(e);
-		}
-	}
+    protected boolean isTransactionActive() throws SystemException, NamingException {
+        int status = getTransaction().getStatus();
+        return status == Status.STATUS_ACTIVE;
+    }
 
-	protected boolean isTransactionActive() throws SystemException, NamingException {
-		int status = getTransaction().getStatus();
-		return status == Status.STATUS_ACTIVE;
-	}
+    protected RuntimeException convertException(Exception e) {
+        if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+        } else {
+            return new RuntimeException(e);
+        }
+    }
 
-	protected RuntimeException convertException(Exception e) {
-		if (e instanceof RuntimeException) {
-			return (RuntimeException) e;
-		} else {
-			return new RuntimeException(e);
-		}
-	}
+    @Override
+    public void persist(Object o) {
+        startOrJoinTransaction();
+        getEntityManager().persist(o);
+    }
 
-	@Override
-	public void persist(Object o) {
-		startOrJoinTransaction();
-		getEntityManager().persist(o);
-	}
+    public UserTransaction getTransaction() throws NamingException {
+        if (transaction == null) {
+            transaction = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
+        }
+        return transaction;
+    }
 
-	public UserTransaction getTransaction() throws NamingException {
-		if (transaction == null) {
-			transaction = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
-		}
-		return transaction;
-	}
+    void setTransaction(UserTransaction transaction) {
+        this.transaction = transaction;
+    }
 
-	void setTransaction(UserTransaction transaction) {
-		this.transaction = transaction;
-	}
+    @Override
+    public <T> T find(Class<T> class1, Object id) {
+        return getEntityManager().find(class1, id);
+    }
 
-	@Override
-	public <T> T find(Class<T> class1, Object id) {
-		return getEntityManager().find(class1, id);
-	}
+    public EntityManager getEntityManager() {
+        if (em.get() == null || !em.get().isOpen()) {
+            em.set(emf.createEntityManager());
+            em.get().joinTransaction();
+            em.get().setFlushMode(FlushModeType.COMMIT);
+        }
+        return em.get();
+    }
 
-	public EntityManager getEntityManager() {
-		if (em.get() == null || !em.get().isOpen()) {
-			em.set(emf.createEntityManager());
-			em.get().joinTransaction();
-			em.get().setFlushMode(FlushModeType.COMMIT);
-		}
-		return em.get();
-	}
+    @Override
+    public void remove(Object s) {
+        getEntityManager().remove(s);
+    }
 
-	@Override
-	public void remove(Object s) {
-		getEntityManager().remove(s);
-	}
+    @Override
+    public void close() {
+        if (em.get() != null && em.get().isOpen()) {
+            em.get().close();
+        }
+        this.startedTransaction = false;
+    }
 
-	@Override
-	public void close() {
-		if (em.get() != null && em.get().isOpen()) {
-			em.get().close();
-		}
-		this.startedTransaction = false;
-	}
+    @Override
+    public void update(Object o) {
+    }
 
-	@Override
-	public void update(Object o) {
-	}
+    @Override
+    public JpaCaseSubscriptionInfo findCaseSubscriptionInfo(CaseSubscriptionKey key) {
+        return getEntityManager().find(JpaCaseSubscriptionInfo.class, key);
+    }
 
-	@Override
-	public JpaCaseSubscriptionInfo findCaseSubscriptionInfo(CaseSubscriptionKey key) {
-		return getEntityManager().find(JpaCaseSubscriptionInfo.class, key);
-	}
+    @Override
+    public void updateCaseSupbscriptionInfo(JpaCaseSubscriptionInfo t) {
+    }
 
-	@Override
-	public void updateCaseSupbscriptionInfo(JpaCaseSubscriptionInfo t) {
-	}
+    @Override
+    public void persistCaseSubscriptionInfo(JpaCaseSubscriptionInfo t) {
+        startOrJoinTransaction();
+        getEntityManager().persist(t);
+    }
 
-	@Override
-	public void persistCaseSubscriptionInfo(JpaCaseSubscriptionInfo t) {
-		startOrJoinTransaction();
-		getEntityManager().persist(t);
-	}
+    @Override
+    public void removeCaseFileItemSubscriptionInfo(JpaCaseFileItemSubscriptionInfo x) {
+        getEntityManager().remove(x);
+    }
 
-	@Override
-	public void removeCaseFileItemSubscriptionInfo(JpaCaseFileItemSubscriptionInfo x) {
-		getEntityManager().remove(x);
-	}
+    @Override
+    public void commit() {
+        try {
+            startOrJoinTransaction();
+            getEntityManager().flush();
+            doCaseFileItemEvents();
+            if (runtimeManager != null) {
+                if (startedTransaction) {
+                    getTransaction().commit();
+                    boolean workItemsProcessed = false;
+                    do {
+                        this.startedTransaction = false;
+                        startOrJoinTransaction();
+                        workItemsProcessed = EventQueues.dispatchWorkItemQueue(runtimeManager.getRuntimeEngine(EmptyContext.get()));
+                        if (workItemsProcessed) {
+                            doCaseFileItemEvents();
+                        }
+                        getTransaction().commit();
+                    } while (workItemsProcessed);
+                    this.startedTransaction = false;
+                }
+            }
+            close();
+        } catch (Exception e) {
+            throw convertException(e);
+        }
+    }
 
-	@Override
-	public void commit() {
-		try {
-			startOrJoinTransaction();
-			getEntityManager().flush();
-			doCaseFileItemEvents();
-			if (startedTransaction) {
-				getTransaction().commit();
-				boolean workItemsProcessed = false;
-				do {
-					this.startedTransaction = false;
-					startOrJoinTransaction();
-					workItemsProcessed = EventQueues.dispatchWorkItemQueue(runtimeManager.getRuntimeEngine(EmptyContext.get()));
-					if (workItemsProcessed) {
-						doCaseFileItemEvents();
-					}
-					getTransaction().commit();
-				} while (workItemsProcessed);
-				this.startedTransaction = false;
-			}
-			close();
-		} catch (Exception e) {
-			throw convertException(e);
-		}
-	}
-
-	private void doCaseFileItemEvents() {
-		while (EventQueues.dispatchCaseFileItemEventQueue(runtimeManager.getRuntimeEngine(EmptyContext.get()))) {
-			AbstractDurableSubscriptionManager.commitSubscriptionsTo(this);
-			getEntityManager().flush();
-		}
-		if (AbstractDurableSubscriptionManager.commitSubscriptionsTo(this)) {
-			getEntityManager().flush();
-		}
-	}
+    private void doCaseFileItemEvents() {
+        if (runtimeManager != null) {
+            while (EventQueues.dispatchCaseFileItemEventQueue(runtimeManager.getRuntimeEngine(EmptyContext.get()))) {
+                AbstractDurableSubscriptionManager.commitSubscriptionsTo(this);
+                getEntityManager().flush();
+            }
+            if (AbstractDurableSubscriptionManager.commitSubscriptionsTo(this)) {
+                getEntityManager().flush();
+            }
+        }
+    }
 
 }
